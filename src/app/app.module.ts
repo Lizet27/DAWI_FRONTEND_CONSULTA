27a9de137a9414112ecb427e9b6237aa50a190e3
ldiana27.ldor <ldiana27.ldor@gmail.com>
2021-10-17 23:47:01 -0500

    Agregar Producto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddConsultaComponent } from './components/add-consulta/add-consulta.component';
 
+import { AddProductoComponent } from './components/add-producto/add-producto.component';
+import { AddCategoriaComponent } from './components/add-categoria/add-categoria.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddConsultaComponent
+    AddConsultaComponent,
+    AddProductoComponent,
+    AddCategoriaComponent
   ],
   imports: [
     BrowserModule,
